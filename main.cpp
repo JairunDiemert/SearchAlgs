4f1161748d20996321eacd50bec11c33021b419b
Jairun Diemert <54635185+JairunDiemert@users.noreply.github.com>
2020-02-28 12:52:36 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//File: main.cpp
+//Author: Jairun Diemert
+//Class: COP3530
+//Project: Class work
+//Description: This is the main source file of my project. 
+
+#include <iostream>
+using namespace std;
+
+#include "Search.h"
+
+int main() {
+
+	Search start;
+	start.Menu();
+	
+}
